7f9950904f3c4463b1907074a86304c3908b3e95
Ichigo <sora1220.119014@outlook.jp>
2020-08-13 14:42:16 +0900

    add: ニックネーム　メールアドレスが表示されるようにした

--- join/check.php
+++ join/check.php
@@ -1,3 +1,12 @@
+<?php
+session_start();
+
+if(!isset($_SESSION['join'])){
+	header('Location: index.php');
+	exit();
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,9 +18,11 @@
     <form action="" method="post">
 		<dl>
 		<dt>ニックネーム</dt>
+		<?php echo htmlspecialchars($_SESSION['join']['name'],ENT_QUOTES); ?>
 		<dd>
 		</dd>
 		<dt>メールアドレス</dt>
+		<?php echo htmlspecialchars($_SESSION['join']['email'],ENT_QUOTES); ?>
 		<dd>
 		</dd>
 		<dt>パスワード</dt>
@@ -19,6 +30,7 @@
 		【表示されません】
 		</dd>
 		<dt>写真など</dt>
+		<img src="../member_picture/<?php echo htmlspecialchars($_SESSION['join']['image'],ENT_QUOTES); ?>" alt="" width="100px" height="100px">
 		<dd>
 		</dd>
 		</dl>
